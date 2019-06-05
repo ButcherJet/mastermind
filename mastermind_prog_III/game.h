dd8f19fbe9ed0887b193e8588bac3a443b719b65
ButcherJet <51417252+ButcherJet@users.noreply.github.com>
2019-06-05 14:54:30 -0300

    Add files via upload

---
+++ mastermind_prog_III/game.h
@@ -0,0 +1,32 @@
+#ifndef GAME_H_
+#define GAME_H_
+
+class game {
+  public:
+    void chooseColor();//choose the number of color in game 4-10
+
+    void chooseSize();//choose the code size 4-6
+
+    void repeatColors();//possible to repeat repeatColors
+
+    void chooseTry();//the number of guessings
+
+
+
+
+
+  private:
+
+
+
+};
+
+
+
+
+
+
+
+
+
+#endif //GAME_H_
